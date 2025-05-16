f88972b14bbdf94870b788f1cf4aa388b9a5c32b
virtualheet <heetvavadiya456@gmail.com>
2025-05-16 14:36:44 +0530

    deploy---next

--- src/data/companieswork.ts
+++ src/data/companieswork.ts
@@ -1,8 +1,8 @@
-const CompaniesWork = [
-    {
-        parmas: "repixelx",
-        name :"Repixelx studio",
-        title1: "Repixelx Studio is a brand freelance development agency based in Jammu, India."
+// const CompaniesWork = [
+//     {
+//         parmas: "repixelx",
+//         name :"Repixelx studio",
+//         title1: "Repixelx Studio is a brand freelance development agency based in Jammu, India."
         
-    }
-]
+//     }
+// ]
